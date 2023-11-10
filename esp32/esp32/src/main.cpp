59d3c39b1dca32e3829801220f2a0f5a7b14f654
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2023-11-10 18:02:41 +0100

    Morse beep

--- esp32/esp32/src/main.cpp
+++ esp32/esp32/src/main.cpp
@@ -1,48 +1,84 @@
 #include <Arduino.h>
 #include <WiFi.h>
+
+#include "ESPAsyncWebServer.h"
+#include "SPIFFS.h"
+
 #define LED 2
+#define BEEP 5
 
 // put function declarations here:
 int myFunction(int, int);
 void initWifi();
 
-WiFiServer server(80);
+String beepMessage = "";
+
+const char* ascii_to_morse[] =
+    {
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",  // 0...9
+        0, 0, 0, 0, 0, 0, 0,
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.",
+        "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..",  // A...Z
+};
+
+AsyncWebServer server(80);
 
 void setup() {
     // put your setup code here, to run once:
     int result = myFunction(2, 3);
     Serial.begin(9600);
     pinMode(LED, OUTPUT);
+    pinMode(BEEP, OUTPUT);
     initWifi();
+
+    server.serveStatic("/", SPIFFS, "/data/");
+
+    server.on("^\\/beep\\/([A-Z ]+)$", HTTP_GET, [](AsyncWebServerRequest* request) {
+        String message = request->pathArg(0);
+        String morse = "";
+        // Iterate over chars and convert to morse
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+            if (c >= 'A' && c <= 'Z') {
+                morse += ascii_to_morse[c];
+            } else if (c == ' ') {
+                morse += "  ";
+            }
+            morse += " ";
+        }
+        beepMessage = morse;
+        request->send(200, "text/plain", morse);
+    });
+
     server.begin();
     Serial.println("Setup done");
 }
 
 void loop() {
-    WiFiClient client = server.available();
-
-    // Check request path
-    if (client) {
-        Serial.println("New client");
-        // Print request path
-        Serial.println(client.readStringUntil('\r'));
-
-        // Answer with "Hello World"
-        client.println("HTTP/1.1 200 OK");
-        client.println("Content-type:text/html");
-        client.println();
-        client.println("<h1>Hello World</h1>");
-        client.println("<p>Test</p>");
-        client.println("<p>Test</p>");
-        // End
-        client.stop();
+    // Get one char from beepMessage
+    if (beepMessage.length() > 0) {
+        char c = beepMessage.charAt(0);
+        beepMessage = beepMessage.substring(1);
+        if (c == '.') {
+            digitalWrite(LED, HIGH);
+            digitalWrite(BEEP, HIGH);
+            delay(100);
+            digitalWrite(LED, LOW);
+            digitalWrite(BEEP, LOW);
+            delay(100);
+        } else if (c == '-') {
+            digitalWrite(LED, HIGH);
+            digitalWrite(BEEP, HIGH);
+            delay(300);
+            digitalWrite(LED, LOW);
+            digitalWrite(BEEP, LOW);
+            delay(100);
+        } else if (c == ' ') {
+            delay(400);
+        }
     }
-
-    Serial.println("Hallo äöß");
-    digitalWrite(LED, HIGH);
-    delay(50);
-    digitalWrite(LED, LOW);
-    delay(50);
 }
 
 // put function definitions here:
@@ -52,7 +88,7 @@ int myFunction(int x, int y) {
 
 void initWifi() {
     WiFi.mode(WIFI_STA);
-    WiFi.begin("HTLGuest", "5BHIF012345");
+    WiFi.begin("SSID", "PASS");
     Serial.println("Connecting to WiFi...");
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
